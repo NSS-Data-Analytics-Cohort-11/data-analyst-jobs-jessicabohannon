22026fcb3ddbaf5eb73ce855a69fd9d4695e6cbf
jessicabohannon <jessicabohannon5@gmail.com>
2024-02-07 08:52:56 -0600

    1-8 complete

--- scripts/jobs.sql
+++ scripts/jobs.sql
@@ -1,6 +1,6 @@
 /* 1. How many rows are in the data_analyst_jobs table? */
 
-SELECT COUNT(*)
+SELECT COUNT(*) AS num_rows
 FROM data_analyst_jobs;
 
 -- Answer: 1793
@@ -15,11 +15,11 @@ LIMIT 10;
 
 /* 3. How many postings are in Tennessee? How many are there in either Tennessee or Kentucky? */
 
-SELECT COUNT(*)
+SELECT COUNT(*) AS tn_jobs
 FROM data_analyst_jobs
 WHERE location = 'TN';
 
-SELECT COUNT(*)
+SELECT COUNT(*) AS tn_and_ky_jobs
 FROM data_analyst_jobs
 WHERE location IN ('TN', 'KY');
 
@@ -27,7 +27,7 @@ WHERE location IN ('TN', 'KY');
 
 /* 4. How many postings in Tennessee have a star rating above 4? */
 
-SELECT COUNT(*)
+SELECT COUNT(*) AS num_of_postings
 FROM data_analyst_jobs
 WHERE location = 'TN'
 	AND star_rating > 4;
@@ -36,7 +36,7 @@ WHERE location = 'TN'
 
 /* 5. How many postings in the dataset have a review count between 500 and 1000? */
 
-SELECT COUNT(*)
+SELECT COUNT(*) AS num_of_jobs
 FROM data_analyst_jobs
 WHERE review_count BETWEEN 500 AND 1000;
 
@@ -51,3 +51,24 @@ GROUP BY location
 ORDER BY avg_rating DESC;
 
 -- Answer: NE
+
+/* 7. Select unique job titles from the data_analyst_jobs table. How many are there? */
+
+SELECT DISTINCT title AS job_title
+FROM data_analyst_jobs
+ORDER BY title;
+
+SELECT COUNT(DISTINCT title) AS num_of_jobs
+FROM data_analyst_jobs;
+
+-- Answer: 881
+
+/* 8. How many unique job titles are there for California companies? */
+
+SELECT COUNT(DISTINCT title) AS num_ca_job_titles
+FROM data_analyst_jobs
+WHERE location = 'CA';
+
+-- Answer: 230
+
+/* 9. Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations? */
